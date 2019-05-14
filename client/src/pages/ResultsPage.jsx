2f8e25909f6a6953014907fe761764a6f6eeb4a7
SujeethJinesh <sujeethjinesh@gmail.com>
2019-05-14 14:04:42 -0700

    Add Page Templates

---
+++ client/src/pages/ResultsPage.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class ResultsPage extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>Results</div>
+        )
+    }
+
+}
+
+export default ResultsPage;
