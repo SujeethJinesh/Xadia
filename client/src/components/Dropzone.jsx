223c19a85327d60dd6cbe9960e8ff86eb0dc3c9e
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2019-05-20 14:42:29 -0700

    Add file size validation and csv only (#19)

--- client/src/components/Dropzone.jsx
+++ client/src/components/Dropzone.jsx
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
+
+// File Upload stuff
 import { FilePond, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
 
-registerPlugin(FilePondPluginImagePreview);
+registerPlugin(FilePondPluginFileValidateSize, FilePondPluginFileValidateType, FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
 class FileUpload extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            files: []
         }
+    }
+
+    handleInit() {
+        console.log('FilePond instance has initialised', this.pond);
     }
 
     render() {
         return (
-            <FilePond allowMultiple={true}/>
+            <FilePond
+                allowMultiple={false}
+                files={this.state.files}
+                oninit={() => this.handleInit() }
+                allowReplace={true}
+                dropOnPage={true}
+                dropValidation={true}
+                instantUpload={false}
+                acceptedFileTypes={['application/vnd.ms-excel','text/csv']}
+                onupdatefiles={(fileItems) => {
+                    // Set current file objects to this.state
+                    this.setState({
+                        files: fileItems.map(fileItem => fileItem.file)
+                    });
+                }}
+                labelIdle={"Only CSV/Excel files and up to 1 mb size"}
+                // This is in bytes
+                maxFileSize={500000}/>
         );
     }
 }
